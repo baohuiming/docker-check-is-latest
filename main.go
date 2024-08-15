c02a6cfccc3d8e3f99acccdf1eb6d3318918ce57
Harry <m@baohuiming.net>
2024-08-15 20:57:19 +0800

    feat: fetch list

--- main.go
+++ main.go
@@ -34,11 +34,16 @@ type Container struct {
 
 type CacheMap map[string]ImageInfo
 
+type GHCRVersion struct {
+	Digest string `json:"name"` // startwith "sha256:"
+	ApiUrl string `json:"url"`
+}
+
 var ghcr_token *string
 var cache CacheMap
 
 // Use registry APIs to fetch image info
-func GetRemoteDockerInfo(image, tag string) (ImageInfo, error) {
+func GetRemoteDockerInfo(image, tag, digest string) (ImageInfo, error) {
 	// [registry-hostname]/[namespace]/[image-name]:[tag]
 	var url string
 	var info ImageInfo
@@ -67,6 +72,7 @@ func GetRemoteDockerInfo(image, tag string) (ImageInfo, error) {
 	case "docker.io":
 		url = fmt.Sprintf("https://registry.hub.docker.com/v2/repositories/%s/%s/tags/%s", namespace, name, tag)
 	case "ghcr.io":
+		// https://docs.github.com/zh/rest/packages/packages?apiVersion=2022-11-28#list-package-versions-for-a-package-owned-by-an-organization
 		if ghcr_token == nil {
 			return info, fmt.Errorf("missing ghcr_token")
 		}
@@ -84,40 +90,87 @@ func GetRemoteDockerInfo(image, tag string) (ImageInfo, error) {
 		return info, fmt.Errorf("not support image %s", image)
 	}
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return info, fmt.Errorf("error while creating request: %s", err)
+	for page := 1; ; page++ {
+		params := ""
+		if registry == "ghcr.io" {
+			params = fmt.Sprintf("?page=%d&per_page=100", page)
+		}
+
+		log.Println("url:", url+params)
+
+		req, err := http.NewRequest("GET", url+params, nil)
+		if err != nil {
+			return info, fmt.Errorf("error while creating request: %s", err)
+		}
+
+		req.Header = headers
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			return info, fmt.Errorf("error while getting %s: %s", url, err)
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return info, fmt.Errorf("error while reading body: %s", err)
+		}
+
+		if registry == "docker.io" {
+			err = json.Unmarshal(body, &info)
+			if err != nil {
+				return info, fmt.Errorf("server error while unmarshalling body: %s", err)
+			}
+
+			if info.MultiplePlatformImageInfoList == nil {
+				return info, fmt.Errorf("error %s", string(body))
+			} else if len(info.MultiplePlatformImageInfoList) == 0 {
+				return info, fmt.Errorf("images is empty for %s:%s", image, tag)
+			}
+
+			cache[image+":"+tag] = info
+
+			return info, nil
+		} else if registry == "ghcr.io" {
+			var resVersions []GHCRVersion
+			err = json.Unmarshal(body, &resVersions)
+			if err != nil {
+				return info, fmt.Errorf("server error while unmarshalling body: %s", err)
+			}
+			// log.Println(resVersions)
+			if len(resVersions) == 0 {
+				return info, fmt.Errorf("no matching images for %s:%s", image, tag)
+			}
+
+			for _, v := range resVersions {
+				if v.Digest == digest {
+					log.Println("v.Digest", v.Digest, digest, "✅")
+					// Get v.ApiUrl
+					req, err := http.NewRequest("GET", v.ApiUrl, nil)
+					if err != nil {
+						return info, fmt.Errorf("error while creating request: %s", err)
+					}
+
+					req.Header = headers
+					resp, err := client.Do(req)
+					if err != nil {
+						return info, fmt.Errorf("error while getting %s: %s", v.ApiUrl, err)
+					}
+					defer resp.Body.Close()
+					body, err = io.ReadAll(resp.Body)
+					if err != nil {
+						return info, fmt.Errorf("error while reading body: %s", err)
+					}
+					// todo
+					log.Println(string(body))
+					return info, nil
+				}
+			}
+
+		}
 	}
 
-	// 将提供的headers添加到请求中
-	req.Header = headers
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return info, fmt.Errorf("error while getting %s: %s", url, err)
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return info, fmt.Errorf("error while reading body: %s", err)
-	}
-
-	err = json.Unmarshal(body, &info)
-	if err != nil {
-		return info, fmt.Errorf("server error while unmarshalling body: %s", err)
-	}
-
-	if info.MultiplePlatformImageInfoList == nil {
-		return info, fmt.Errorf("error %s", string(body))
-	} else if len(info.MultiplePlatformImageInfoList) == 0 {
-		return info, fmt.Errorf("images is empty for %s:%s", image, tag)
-	}
-
-	cache[image+":"+tag] = info
-
-	return info, nil
 }
 
 // Use docker client API to fetch portainer list
@@ -175,7 +228,7 @@ func main() {
 			imageTag = strings.Split(imageName, ":")[1]
 			imageName = strings.Split(imageName, ":")[0]
 		}
-		latest, err := GetRemoteDockerInfo(imageName, "latest")
+		latest, err := GetRemoteDockerInfo(imageName, "latest", imageDigest)
 		if err != nil {
 			log.Println("Unable to get remote docker tag:", name, imageName, err)
 			continue
@@ -189,7 +242,7 @@ func main() {
 			continue
 		}
 
-		current, err := GetRemoteDockerInfo(imageName, imageTag)
+		current, err := GetRemoteDockerInfo(imageName, imageTag, imageDigest)
 
 		if err != nil {
 			log.Println("Unable to get remote docker tag:", err)
